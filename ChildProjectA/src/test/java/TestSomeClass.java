5c40b1fa88e3c4e33c7d0e23ae377d8d5c13afae
leonardo <lpina@uolinc.com>
2020-05-02 16:57:31 -0300

    simple hello world and test

--- ChildProjectA/src/test/java/TestSomeClass.java
+++ ChildProjectA/src/test/java/TestSomeClass.java
@@ -1,8 +1,20 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.*;
 
 public class TestSomeClass {
 
     @Test
-    public void nothing(){
+    public void nothing() throws IOException {
+        SomeClass sc = new SomeClass();
+        PrintStream sysout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        sc.greetWorld();
+        Assertions.assertEquals("Hello World!\n", out.toString());
+
+        System.setOut(sysout);
     }
 }
